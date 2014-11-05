f8f4d98d44513df6afd97fedfbf6011dfd6b98e1
diega <daperez@gmail.com>
2014-11-05 19:15:21 -0300

    Loc tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,8 +7,34 @@ angular.module('starter.controllers', [])
   $scope.pos = Pos.all();
 })
 
-.controller('LocCtrl', function($scope) {
+.controller('LocCtrl', function($scope, $ionicLoading) {
+    
+     
+     google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        
+         
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
   
+        $scope.map = map;
+    });
+     
 })
 
 .controller('PosDetailCtrl', function($scope, $stateParams, Pos) {
